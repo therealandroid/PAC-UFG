c3e66b3dce6ba7710860e51f0e8a08eb7e6809d5
diogojayme <diogojme@gmail.com>
2019-04-21 10:42:09 -0300

    fix #4 fix #2

--- src/main/java/com/ufg/br/pac/controllers/PacoteController.java
+++ src/main/java/com/ufg/br/pac/controllers/PacoteController.java
@@ -1,34 +1,33 @@
 package com.ufg.br.pac.controllers;
 
 import com.ufg.br.pac.entities.Pacote;
+import com.ufg.br.pac.repository.PacoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PacoteController {
 
-    //In memory list
-    private List<Pacote> pacotesCriados = new ArrayList<>();
+    @Autowired
+    private PacoteRepository pacoteRepository;
 
     @GetMapping("/create")
     public String create(Model model) {
         Pacote pacote = new Pacote();
         model.addAttribute("pacote", pacote);
-        model.addAttribute("pacotes", pacotesCriados);
+        model.addAttribute("pacotes", pacoteRepository.findAll());
         return "criar_pacote";
     }
 
     @PostMapping("/create")
     public String create(@ModelAttribute Pacote pacote) {
-
-        pacotesCriados.add(0, pacote);
-
+        pacoteRepository.save(pacote);
         return "redirect:/create";
     }
 
---
+++ src/main/java/com/ufg/br/pac/repository/PacoteRepository.java
@@ -0,0 +1,8 @@
+package com.ufg.br.pac.repository;
+
+import com.ufg.br.pac.entities.Pacote;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PacoteRepository extends CrudRepository<Pacote, Integer> {
+
+}
